d96d160c0b10bc4c15fa889afc739c1159785513
monwar23 <monwar2345@gmail.com>
2024-03-13 18:47:22 +0600

    data fetched

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
 import Header from './components/Header/Header'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Header></Header>
-      <Blogs></Blogs>
+      <div className='md:flex '>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-
+import profile from '../../assets/images/profile.png';
 
 const header = () => {
     return (
-        <div>
-            <h3 className="text-4xl font-bold">Knowledge Cafe</h3>
-        </div>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
